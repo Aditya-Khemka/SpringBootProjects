a698e5bb3f0e654fe0380845337b47baacc6a619
Aditya Khemka <khemkaaditya48@gmail.com>
2026-06-10 00:26:37 +0530

    Configured the banking app project , env setup

---
+++ baning-app/baning-app/src/main/java/net/aditya/baning_app/BaningAppApplication.java
@@ -0,0 +1,13 @@
+package net.aditya.baning_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaningAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaningAppApplication.class, args);
+	}
+
+}
---
+++ baning-app/baning-app/src/test/java/net/aditya/baning_app/BaningAppApplicationTests.java
@@ -0,0 +1,13 @@
+package net.aditya.baning_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaningAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
