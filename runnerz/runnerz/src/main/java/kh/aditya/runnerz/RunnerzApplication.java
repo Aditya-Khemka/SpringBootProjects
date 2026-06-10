7fdee7927eaed622d1bf2cad1fd82dac3e6e7bce
Aditya Khemka <khemkaaditya48@gmail.com>
2026-06-10 23:23:03 +0530

    usage of bean

--- runnerz/runnerz/src/main/java/kh/aditya/runnerz/RunnerzApplication.java
+++ runnerz/runnerz/src/main/java/kh/aditya/runnerz/RunnerzApplication.java
@@ -2,17 +2,50 @@ package kh.aditya.runnerz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 
 @SpringBootApplication
 public class RunnerzApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RunnerzApplication.class, args) ;
 
+
+		//SpringApplication.run(RunnerzApplication.class, args) ;
+
+		/*
 		//Aam Zindagi
 		var Welcome = new Welcome();
 		System.out.println(Welcome.getWelcomeMessage());
+		*/
+
+
+		//Mentos Zindagi
+
+		//This scans the app for all @Component (creates their objects and stores them)
+		ConfigurableApplicationContext context = SpringApplication.run(RunnerzApplication.class, args);
+		//The variable context represents the container in which all the objects are stored
+
+
+		//bean is just an instance of the class using some metadata managed by spring itself
+		// get an object using bean
+		Object obj = context.getBean("welcome"); // this gets the bean of class Welcome
+		// unlike java, obj does not represent an object of class Welcome
+
+		//option 1 : typecast to get an object
+		Welcome welcome1 = (Welcome) context.getBean("welcome");
+		System.out.println(welcome1.getWelcomeMessage());
+
+		//option 2 : same as option 1 but cleaner
+		Welcome welcome2 = context.getBean(Welcome.class);
+		System.out.println(welcome2.getWelcomeMessage());
+
+		//note that bean is lowercase by default. Welcome will throw an error (we can force it to use Welcome, but that for later)
+
+		//also check
+		System.out.println(welcome1 == welcome2); //true
+		//because Spring returns the same object by default.
+
 	}
 
 }
--- runnerz/runnerz/src/main/java/kh/aditya/runnerz/Welcome.java
+++ runnerz/runnerz/src/main/java/kh/aditya/runnerz/Welcome.java
@@ -1,11 +1,15 @@
 package kh.aditya.runnerz;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 @Component
+//Component creates Welcome welcome = new Welcome();
 public class Welcome {
 
+    @Bean
     public String getWelcomeMessage() {
         return "kh.aditya.runnerz.Welcome to Runnerz!";
     }
 }
+
