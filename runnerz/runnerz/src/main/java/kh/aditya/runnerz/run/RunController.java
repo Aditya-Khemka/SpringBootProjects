06d8fbf24ddcfa905fb367c1a663bd18093c6005
Aditya Khemka <khemkaaditya48@gmail.com>
2026-06-13 19:54:42 +0530

    a simple JDBS Client. Grab all Runs from the DB (GET)

--- runnerz/runnerz/src/main/java/kh/aditya/runnerz/run/RunController.java
+++ runnerz/runnerz/src/main/java/kh/aditya/runnerz/run/RunController.java
@@ -6,5 +6,21 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
+@RestController
+@RequestMapping("/api")
 public class RunController {
+    //Again create an instance of RunRepository
+    private final RunRepository runRepository;
+
+    //Initalise it
+    public RunController(RunRepository runRepository) {
+        this.runRepository = runRepository;
+    }
+
+    //And get all the runs using the findAll function
+    @GetMapping("/allRuns")
+    List<Run> findAll() {
+        return runRepository.findAll();
+    }
+
 }
--- runnerz/runnerz/src/main/java/kh/aditya/runnerz/run/RunRepository.java
+++ runnerz/runnerz/src/main/java/kh/aditya/runnerz/run/RunRepository.java
@@ -1,6 +1,8 @@
 package kh.aditya.runnerz.run;
 
 import jakarta.annotation.PostConstruct;
+import org.slf4j.ILoggerFactory;
+import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
@@ -13,5 +15,19 @@ import java.util.Optional;
 // now we can access it using Spring methods in other classes
 @Repository
 public class RunRepository {
+
+    //Spring will autoconfigure JDBC so we can talk to a DB
+    private final JdbcClient jdbcClient;
+    public RunRepository(JdbcClient jdbcClient) {
+        this.jdbcClient = jdbcClient;
+    }
+
+    //List all runs (this time get data from a db)
+    public List<Run> findAll() {
+        return jdbcClient.sql("select * from run") //Run the SQL command
+                .query(Run.class) //get the result (and map to Run type)
+                .list(); //convert it to list
+    }
+
 
 }
